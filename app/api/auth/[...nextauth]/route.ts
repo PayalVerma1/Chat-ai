2ae622e3ea44c5e4a52711fada047f2abbdf9cb4
PayalVerma1 <payalvermaseema@gmail.com>
2025-05-13 23:31:06 +0530

    Auth error fixed

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,15 @@
-import GoogleProvider from "next-auth/providers/google";
+// app/api/auth/[...nextauth]/route.ts
+
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
 const handler = NextAuth({
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID ?? ' ',
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? ' ',
-        }),
-    ],
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID ?? "",
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET ?? "",
+    })
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
 });
 export { handler as GET, handler as POST };
