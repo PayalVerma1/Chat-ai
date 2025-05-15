1e7c48cd973ee356428cb650309e771870bb7783
PayalVerma1 <payalvermaseema@gmail.com>
2025-05-15 23:53:36 +0530

    Generated Prisma Client

---
+++ lib/db.ts
@@ -0,0 +1,9 @@
+// import { Configuration, OpenAI } from 'openai';
+
+// const configuration = new Configuration({
+//     apiKey: process.env.API_KEY,
+// });
+
+// const openai = new OpenAI(configuration);
+
+// export default openai;
--- lib/openai.tsx
+++
@@ -1,9 +0,0 @@
-// import { Configuration, OpenAI } from 'openai';
-
-// const configuration = new Configuration({
-//     apiKey: process.env.API_KEY,
-// });
-
-// const openai = new OpenAI(configuration);
-
-// export default openai;
