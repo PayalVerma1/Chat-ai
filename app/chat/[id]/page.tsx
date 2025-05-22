50f8505bb2893f3cddd214b6a21167b36748faa5
PayalVerma1 <payalvermaseema@gmail.com>
2025-05-22 23:30:22 +0530

    zustand added and used

---
+++ app/chat/[id]/page.tsx
@@ -0,0 +1,39 @@
+"use client";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+import { Fullchat } from "@/store/chatStore";
+import { useChatStore } from "@/store/chatStore";
+import axios from "axios";
+export default function ChatPage() {
+  const { id } = useParams();
+  const { chat, addChat, clearChat } = useChatStore();
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    const fetchChat = async () => {
+      try {
+        const res = await axios.get(`/api/chat?id=${id}`);
+        const data = res.data;
+        if (data.data) {
+          addChat(data.data);
+        }
+      } catch (error) {
+        console.log("Error:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchChat();
+  },[id]);
+  if (loading) return <div>Loading...</div>;
+  return (
+     <div>
+      <h1>Chat Page</h1>
+      {chat && chat.exchanges.map((pair) => (
+        <div key={pair.id}>
+          <p><strong>You:</strong> {pair.prompt}</p>
+          <p><strong>AI:</strong> {pair.response}</p>
+        </div>
+      ))}
+    </div>
+  );
+}
--- store/chatStore.tsx
+++ store/chatStore.tsx
@@ -1,10 +1,14 @@
 import { create } from 'zustand';
-import {Chat} from "@prisma/client"
+import {Chat,Pair} from "@prisma/client"
 import { persist } from 'zustand/middleware';
+export type Fullchat=Chat & {
+    exchanges:Pair[]
+}
 interface ChatStore {
-    //chat is a object of type Chat
-    chat:Chat|null;
-    addChat: (chats: Chat) => void;
+    //chat is a object of type FullChat
+    chat:Fullchat|null;
+    //chats is the array of type FullChat and chat is a array of pair 
+    addChat: (chats: Fullchat) => void;
     clearChat: () => void;
 }
 
@@ -12,11 +16,12 @@ export const useChatStore=create<ChatStore>()(
     persist(
        (set)=>({
         chat:null,
-        addChat:(chats: Chat) =>set(()=>({chat:chats})),
-         clearChat: () => set(()=>({chat:null})),
+        //chat:data.data passed chat/[id]
+        addChat: (chats: Fullchat) => set(() => ({ chat: chats })),
+        clearChat: () => set(() => ({ chat: null })),
        }),
        {
-        name:"Chat Store"
+        name: "Chat Store"
        }
     )
 )
