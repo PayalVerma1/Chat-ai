b05360ab199f7a6baed137eb49d64d500715c163
PayalVerma1 <payalvermaseema@gmail.com>
2025-06-01 00:35:15 +0530

    Sidebar fix

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,13 @@
 import AppBar from "./components/AppBar";
 // import InputBox from "./components/InputBox";
 import ChatPage from "./chat/[id]/page";
-import SideBar from "./components/SideBar";
+
 export default function Home() {
   return (
     <div className="flex h-screen ">
      
    
-    <SideBar />
+   
      <ChatPage/>
     </div>
   );
