4d4c86d7eb26c857ce2bca6688398800ead08a32
PayalVerma1 <payalvermaseema@gmail.com>
2025-07-02 15:16:58 +0530

    Signin Icon added

--- app/components/AppBar.tsx
+++ app/components/AppBar.tsx
@@ -1,7 +1,23 @@
 import SignIn from "./client-components/Signin";
 export default function AppBar() {
   return (
-    <div className="flex justify-between items-center ">
+    <div className="flex ml-6 items-center ">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        width="24"
+        height="24"
+        viewBox="0 0 24 24"
+        fill="none"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        className="lucide lucide-log-in size-4"
+      >
+        <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+        <polyline points="10 17 15 12 10 7"></polyline>
+        <line x1="15" x2="3" y1="12" y2="12"></line>
+      </svg>
       <SignIn />
     </div>
   );
